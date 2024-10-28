ae551a0355b176387b4e29106c83cafac7a06d68
Lidne <play.gt.play@yandex.ru>
2024-10-28 21:58:40 +0300

    removed something

--- src/task1.pl
+++ src/task1.pl
@@ -26,7 +26,3 @@ merge([], L, L).
 merge(L, [], L).
 merge([H1|T1], [H2|T2], [H1|R]) :- H1 =< H2, merge(T1, [H2|T2], R).
 merge([H1|T1], [H2|T2], [H2|R]) :- H1 > H2, merge([H1|T1], T2, R).
-
-example(X, Y, Z) :-
-    delete_all(3, X, X1),
-    merge(X1, Y, Z).
